120cfef35f96865eb9741baea2e48d61d37e6eb1
Garry Cyre <garry@cyre.ca>
2016-04-15 18:42:17 -0400

    messing around

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'win-base'
 maintainer       'Garry Cyre'
 maintainer_email 'garry@cyre.ca'
 license          'All rights reserved'
-description      'Installs/Configures win-bse'
+description      'Installs/Configures win-base'
 long_description 'Installs/Configures win-base'
 
 version          '0.1.0'
